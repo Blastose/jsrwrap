39615741abe57f8cc964106364bdbecc3f2c7002
Blastose <blastoisejay@gmail.com>
2022-09-22 13:51:26 -0700

    Add node-fetch package and make it run with jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node'
+	preset: 'ts-jest/presets/js-with-ts-esm',
+	testEnvironment: 'node',
+	transformIgnorePatterns: [
+		'node_modules/(?!(node-fetch|data-uri-to-buffer|fetch-blob|formdata-polyfill)/)'
+	]
 };
