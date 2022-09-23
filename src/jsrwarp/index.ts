cb1323a547f7f33cb26cf055552e177ccfd51058
Blastose <blastoisejay@gmail.com>
2022-09-23 13:04:09 -0700

    Create wrapper and add tests for default authorization flow

---
+++ src/jsrwarp/index.ts
@@ -0,0 +1,92 @@
+import fetch from 'node-fetch';
+
+type Scope =
+	| 'identity'
+	| 'edit'
+	| 'flair'
+	| 'history'
+	| 'modconfig'
+	| 'modflair'
+	| 'modlog'
+	| 'modposts'
+	| 'modwiki'
+	| 'mysubreddits'
+	| 'privatemessages'
+	| 'read'
+	| 'report'
+	| 'save'
+	| 'submit'
+	| 'subscribe'
+	| 'vote'
+	| 'wikiedit'
+	| 'wikiread';
+
+type OAuthConfig = {
+	clientId: string;
+	clientSecret: string;
+	state: string;
+	responseType: 'code' | 'token';
+	redirectUri: string | null;
+	duration: 'temporary' | 'permanent';
+	scope: Scope[];
+	grantType: 'authorization_code' | 'refresh_token';
+	accessToken: string;
+	refreshToken: string;
+};
+
+class Jsrwrap {
+	accessToken: string;
+
+	constructor(accessToken: string) {
+		this.accessToken = accessToken;
+	}
+
+	static createAuthUrl(
+		clientId: string,
+		state: string,
+		redirectUri: string,
+		duration: 'temporary' | 'permanent',
+		scope: Scope[]
+	): string {
+		let scopes = scope.join(' ');
+		scopes = encodeURIComponent(scopes);
+		return `https://www.reddit.com/api/v1/authorize?client_id=${clientId}&response_type=code&state=${state}&redirect_uri=${redirectUri}&duration=${duration}&scope=${scopes}`;
+	}
+
+	static async fromAuthCode(
+		clientId: string,
+		clientSecret: string,
+		redirectUri: string,
+		code: string
+	): Promise<Jsrwrap> {
+		const clientIdAndSecret = Buffer.from(`${clientId}:${clientSecret}`);
+		const base64EncodedClientIdAndSecret = clientIdAndSecret.toString('base64');
+
+		const res = await fetch('https://www.reddit.com/api/v1/access_token', {
+			method: 'POST',
+			headers: {
+				Authorization: `Basic ${base64EncodedClientIdAndSecret}`,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			},
+			body: `grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`
+		});
+
+		type jsonResponse = {
+			access_token: string;
+			token_type: string;
+			expires_in: string;
+			scope: string;
+			refresh_token: string;
+		};
+
+		if (res.status !== 200) {
+			throw new Error('Invalid credentials');
+		}
+
+		const resJson = (await res.json()) as jsonResponse;
+
+		return new Jsrwrap(resJson.access_token);
+	}
+}
+
+export { Jsrwrap };
---
+++ tests/jsrwrap.test.ts
@@ -0,0 +1,33 @@
+import { Jsrwrap } from '../src/jsrwarp/index';
+
+describe('Jsrwrap static methods', () => {
+	it('should generate a valid authorization URL', () => {
+		expect(
+			Jsrwrap.createAuthUrl(
+				'T_OJ428Xl6SYT52iyI8VKA',
+				'state_123',
+				'http://localhost:5173',
+				'permanent',
+				['edit', 'history']
+			)
+		).toBe(
+			'https://www.reddit.com/api/v1/authorize?client_id=T_OJ428Xl6SYT52iyI8VKA&response_type=code&state=state_123&redirect_uri=http://localhost:5173&duration=permanent&scope=edit%20history'
+		);
+	});
+});
+
+describe('Jsrwrap token retrieval', () => {
+	// Code is one-time use so repeated calls to this test should fail
+	// You can get a code from the above link and extracting the code from the query param
+	test('reddit returns a JSON with an access token when authenticating with code', async () => {
+		const reddit = await Jsrwrap.fromAuthCode(
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			process.env.CLIENT_ID!,
+			// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+			process.env.CLIENT_SECRET!,
+			'http://localhost:5173',
+			'flAqmMMLowCKKagZLMRR72yJcs7rrg'
+		);
+		expect(reddit).toBeInstanceOf(Jsrwrap);
+	});
+});
