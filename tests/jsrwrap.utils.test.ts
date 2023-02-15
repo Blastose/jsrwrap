4abcdea08fcfaf1f122b3ac007e4382e2062d56d
Blastose <blastoisejay@gmail.com>
2023-02-15 15:24:04 -0800

    Add function to build query strings

---
+++ tests/jsrwrap.utils.test.ts
@@ -0,0 +1,13 @@
+import { buildQueryString } from '../src/jsrwarp/utils/buildQueryString';
+
+describe('buildQueryString', () => {
+	it('should get build the query string', () => {
+		const result = buildQueryString({ t: 'all', after: 't3qwert', count: 24 });
+		expect(result).toBe('t=all&after=t3qwert&count=24');
+	});
+
+	it('should return an empty string when params are undefined', () => {
+		const result = buildQueryString(undefined);
+		expect(result).toBe('');
+	});
+});
