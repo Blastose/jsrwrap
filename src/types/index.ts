931e1cce83b8da3583dd7dc102ce749d7c90a0f7
Blastose <blastoisejay@gmail.com>
2023-03-24 20:24:11 -0700

    Move types exports to ./types (#12)

---
+++ src/types/index.ts
@@ -0,0 +1,34 @@
+export type {
+	Awardings,
+	Comment,
+	CommentFull,
+	CommentResponse,
+	MoreChildrenResponse,
+	Replies,
+	RepliesResponse,
+	ResizedIcon
+} from './comment.js';
+export type { Features, Karma, Mweb, Prefs, SubredditUser, Trophy, User } from './redditAccount.js';
+export type {
+	ListingResponse,
+	ListingResponseFull,
+	MoreResponse,
+	TResponse
+} from './redditAPIResponse.js';
+export type {
+	FlairRichtext,
+	Gildings,
+	ImagePreview,
+	Media,
+	MediaEmbed,
+	Preview,
+	SecureMediaEmbed,
+	Sort,
+	Source,
+	SubmissionData,
+	SubredditType,
+	AlbumEntry,
+	GalleryData,
+	MediaMetadata
+} from './submission.js';
+export type { CommentContributionSettings, SubredditData } from './subreddit.js';
